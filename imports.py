1a06df108ab27f83d502172fdffc014509133cdb
Brunowar12 <Brunowar12@users.noreply.github.com>
2024-06-02 15:28:38 +0300

    darktheme and yaml done

--- imports.py
+++ imports.py
@@ -4,7 +4,38 @@ import sys
 import datetime
 import sqlite3
 
-from PyQt6.QtCore import QTimer, Qt, QSize, QFile, QTextStream, QIODevice, pyqtSignal, QEvent, QDateTime
-from PyQt6.QtGui import QColor, QStandardItemModel, QStandardItem, QIcon
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsDropShadowEffect, QMessageBox, QLineEdit, QPushButton, QWidget
-from PyQt6 import uic
+from PyQt6.QtCore import (
+    QTimer,
+    Qt,
+    QSize,
+    QFile,
+    QTextStream,
+    QIODevice,
+    pyqtSignal,
+    QEvent,
+    QDateTime,
+    QSettings,
+)
+from PyQt6.QtGui import (
+    QColor,
+    QStandardItemModel,
+    QStandardItem,
+    QIcon,
+    QFont,
+    QFontDatabase,
+)
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QGraphicsDropShadowEffect,
+    QMessageBox,
+    QLineEdit,
+    QPushButton,
+    QWidget,
+    QDialog,
+    QVBoxLayout,
+    QPushButton,
+    QCheckBox,
+    QSizePolicy,
+)
+from PyQt6 import uic, QtWidgets, QtCore
